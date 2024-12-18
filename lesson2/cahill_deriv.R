0e8c98fe1ecf54bf2fc87113d604453064f777b9
Chris <cahill11@msu.edu>
2024-12-18 10:09:56 -0500

    bump cahill symbolic deriv, uncertainty

---
+++ lesson2/cahill_deriv.R
@@ -0,0 +1,17 @@
+# Define the function
+f <- function(x) 2 + 0.5 * x + sin(x)
+# ----------------------------------------------------------
+# Symbolic differentiation
+library(Deriv)
+f_prime <- Deriv(f, "x") # First derivative
+f_double_prime <- Deriv(f_prime, "x") # Second derivative
+f_prime(1)
+f_double_prime(1)
+#-----------------------------------------------------------
+# Automatic differentiation
+library(RTMB)
+F <- MakeTape(f, numeric(1)) # two valued input
+f_prime <- F$jacfun()
+f_prime(1)
+f_double_prime <- f_prime$jacfun()
+f_double_prime(1)
